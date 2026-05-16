739d58ffa19f7b85d4e8713de6dc4f50f2c0d77e
kensy21 <quirozmatiaskensy@gmail.com>
2026-05-15 20:38:10 -0400

    Create bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,25 @@
+import random
+
+
+def contraseña(pass_length):
+    elements = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(1, 2)
+    if flip == 1:
+        return "cara o crus...cara"
+    else:
+        return "cara o crus...crus"
+ 
