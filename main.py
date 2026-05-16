97a318e2d4714346accfe42caec556b19b84072f
kensy21 <quirozmatiaskensy@gmail.com>
2026-05-15 20:10:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
+from discord.ext import commands
+from bot_logic import *
+import discord
 import random
 
-caracter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
-contraseña_longitu = int(input("Introdusca la longuitud de su contraeña"))
+intents = discord.Intents.default()
+intents.message_content = True
 
-contraseña = ""
+#$
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-for i in range(contraseña_longitu):
+@bot.event
+async def encendido():
 
-    contraseña += random.choice(caracter)
+    print(f'Tu bot {bot.user} esta en linea')
 
-print(contraseña)
+@bot.command()
+async def saludo(ctx):
+
+    await ctx.send('Hola que mas manitooooo')
+
+@bot.command()
+async def comandos(ctx):
+    await ctx.send("Estos son los comandos que tengo"
+                   "$saludo:"
+                    "$despedida: aqui tienes que poener un mensaje como chao"
+                    "$hola"
+                    "$emodji"
+                    "$moneda"
+                    "$cont")
+
+@bot.command()
+async def despedida (ctx, *, mensaje:str):
+
+    #novemo
+
+    mensaje = mensaje.lower().strip()
+
+    if mensaje in 'chao':
+
+        await ctx.send('Chao se me cuida')
+    
+    elif mensaje in 'bye':
+
+        await ctx.send('Goodbye')
+
+    elif mensaje in 'novemo':
+
+        await ctx.send('nosrevimos')
+
+@bot.command()
+async def operacion(ctx, left: int, mensaje:str, right: int):
+    if mensaje == "*" or mensaje == "x":
+        await ctx.send( left * right)
+    elif mensaje == "+":
+        await ctx.send( left + right)
+    elif mensaje == "-":
+        await ctx.send( left - right)
+    elif mensaje == "/":
+        await ctx.send( left / right)
+        
+E
+token=""
+
+bot.run(token)
+
